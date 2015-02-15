413cc17b928b2a6e6aa6a931bf66e39c98b5608c
nediks <cherepov.igor@gmail.com>
2015-02-16 01:49:29 +0400

    Update
    Путь до DB

--- np_station/settings.py
+++ np_station/settings.py
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': rel('C:/np_station/np_station/DB/npbase.db'),            # Or path to database file if using sqlite3.
+        'NAME': rel('C:\np_station\NPStation\DB\npbase.db'),            # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -65,7 +65,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = rel('C:/np_station/np_station/np_server/static')
+STATIC_ROOT = rel('C:\np_station\NPStation\np_server\static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
